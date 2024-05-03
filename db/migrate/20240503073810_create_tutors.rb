680a8df93acd8deee7b563c76e174ea35288f5cf
Your Name <you@example.com>
2024-05-03 13:11:47 +0530

    defined controller and models

---
+++ db/migrate/20240503073810_create_tutors.rb
@@ -0,0 +1,14 @@
+class CreateTutors < ActiveRecord::Migration[7.0]
+  def change
+    create_table :tutors do |t|
+      t.string :name
+      t.string :email
+      t.text :about
+      t.integer :experience
+      t.string :specialization
+      t.references :course, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
