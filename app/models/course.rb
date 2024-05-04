ce3708faae60ac2c499a0221de3955263946b88d
piyushbokde123 <102504781+piyushbokde123@users.noreply.github.com>
2024-05-04 22:11:51 +0530

    fixed code

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,15 +1,15 @@
 class Course < ApplicationRecord
-	has_many :tutors, dependent: :destroy
-	validates :title, :description, :duration, :price, presence: true
-	validates :tutors, presence: true
-	validate :unique_tutor_emails
-	
-	enum level: { Beginner: 0, Intermediate: 1, Advanced: 2 }
-	accepts_nested_attributes_for :tutors
+  has_many :tutors, dependent: :destroy
+  validates :title, :description, :duration, :price, presence: true
+  validates :tutors, presence: true
+  validate :unique_tutor_emails
+
+  enum level: { Beginner: 0, Intermediate: 1, Advanced: 2 }
+  accepts_nested_attributes_for :tutors
   
   private
 
-	def unique_tutor_emails
+  def unique_tutor_emails
     return unless tutors.any? && tutors.map(&:email).uniq.size != tutors.size
 
     errors.add(:base, 'Each course must have tutors with unique email addresses')
